ee29e71098defdd2a7333d7280d50f4b547a6cb7
PROEO6EEXP <ekke.jakob.mesila@tktk.ee>
2024-10-12 01:04:53 +0300

    my week6 code

---
+++ test1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.Design;
+
+string userPIN, userName, password;
+Console.WriteLine("Enter PIN:");
+userPIN = Console.ReadLine();
+
+
+if (userPIN == "1234")
+{
+    Console.WriteLine("Enter your username:");
+    userName = Console.ReadLine();
+    Console.WriteLine("Enter your password:");
+    password = Console.ReadLine();
+
+    if (userName == "admin1" && password == "admin1234")
+    {
+        Console.WriteLine("Welcome!");
+    }
+    else
+    {
+        Console.WriteLine("Invalid login credentials.");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid PIN");
+}
